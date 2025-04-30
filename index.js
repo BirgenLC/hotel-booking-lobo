bd2de3a5e98e5ecf6bc1efc4f4a53adfa4444896
Birgen <birgen156@gmail.com>
2025-04-30 11:37:55 +0300

    session

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
 const mysql = require("mysql");
+const bcrypt = require ("bycrypt")
+const session = require("session")
 
 const dbConnection = mysql.createConnection({
   host: "localhost",
@@ -8,7 +10,9 @@ const dbConnection = mysql.createConnection({
   password: "",
   database: "the lobo village",
 });
-
+app.use(sessionManager){
+  secret:"secret"
+}
 const app = express();
 
 //middleware
@@ -33,7 +37,21 @@ app.get("/", (req, res) => {
     }
   });
 });
+ 
+app.get("/login",(req,res)=>{
+  const{email,password} =req.body});
 
+  dbConnection.query(`SELECT * FROM users WHERE email =?`,[email],async (error,results)=>{
+    if(err || results.length===0) return res.status(404).send("invalind credentials");
+  const user = results[0]
+  const validPass = await bycrpt.compare(password,user.password);
+  if(!validPass) return res.status(401).send("incorrect password")
+    req.session.user = {
+  id:user.id,
+  email:user.email,
+  role:user.role,
+};
+})
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
